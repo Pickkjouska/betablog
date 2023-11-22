1e5d9283273f5b50d5e081d9d97ace739d106e20
Pickkjouska <104264600+Pickkjouska@users.noreply.github.com>
2023-11-22 18:17:09 +0800

    新增页面跳转的过渡动画

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,4 +14,11 @@ export default defineNuxtConfig({
       ],
     },
   },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  css: [
+    '~/assets/style/transitions.css',
+  ]
+  
 })
