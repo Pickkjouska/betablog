9960482f067809c17c9075b151f56a6cdb07bc05
Pickkjouska <104264600+Pickkjouska@users.noreply.github.com>
2023-11-22 17:32:14 +0800

    文章页面新增返回首页

--- components/MessageNote.vue
+++ components/MessageNote.vue
@@ -1,6 +1,9 @@
 <template>
   <div class=" mx-auto max-w-screen-sm box-border mt-10 rounded-lg overflow-hidden opacity-80  
   shadow-lg p-3 dark:bg-gray-900">
+  
+    <Backpages/>
+
     <div class="slide-enter">
       <slot/>
     </div>
---
+++ components/backpages.vue
@@ -0,0 +1,15 @@
+<template>
+    <UButton icon="i-heroicons-arrow-left-solid" size="xl" :padded="false" color="gray"
+    variant="link" label="返回首页" :trailing="false" @click="goBack"/>
+
+</template>
+  
+<script setup lang="ts">
+
+const goBack = () => {
+  const router = useRouter();
+  router.push('/');
+};
+
+</script>
+  
