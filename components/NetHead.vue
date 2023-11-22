fff6bdd013d495a169f5872e56664814300b70f3
Pickkjouska <104264600+Pickkjouska@users.noreply.github.com>
2023-11-22 16:41:08 +0800

    添加返回顶部按钮

--- components/NetHead.vue
+++ components/NetHead.vue
@@ -7,9 +7,13 @@
     <ColorButton/>
     </div>
     <div class="grow h-14"></div>
+    <div class="flex-none h-12 ">
+      <Returntop/>
+    </div>
     <div class="flex-none w-30 h-12 ">
       <Search/> 
     </div>
+    
   </div>
 </template> 
 
---
+++ components/returntop.vue
@@ -0,0 +1,28 @@
+<template>
+  <UButton lable="button" @click="scrollToTop" icon="i-heroicons-arrow-up"
+    color="gray" variant="link" size="xl" square v-if="showButton" 
+    class="rounded-md"/>
+</template>
+  
+<script setup lang="ts">
+const showButton = ref(false);
+
+const handleScroll = () => {
+  showButton.value = window.scrollY > 100; // 显示按钮条件，你可以根据需要调整
+};
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // 平滑滚动
+  });
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
