1d69bd55422523e02da93de8da7eef99e149f0e6
Pickkjouska <104264600+Pickkjouska@users.noreply.github.com>
2023-11-23 18:13:39 +0800

    增加了giscus评论区

--- components/MessageNote.vue
+++ components/MessageNote.vue
@@ -7,10 +7,27 @@
     <div class="slide-enter">
       <slot/>
     </div>
+    <Giscus 
+        repo="Pickkjouska/betablog"
+        repo-id="R_kgDOKvDD6w"
+        category="General"
+        category-id="DIC_kwDOKvDD684CbL0d"
+        mapping="title"
+        term="Welcome to my blog!"
+        reactions-enabled="1"
+        emit-metadata="1"
+        input-position="top"
+        theme="light"
+        lang="zh-CN"
+        crossorigin="anonymous"
+        loading="lazy"  
+        >
+    </Giscus >
   </div>
 </template>
 
 <script setup lang="ts">
+import  Giscus  from '@giscus/vue';
 const isLoad = ref(false)
 onMounted(() => {
   isLoad.value = true;
